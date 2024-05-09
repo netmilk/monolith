7ce6a84651d31a59347e0e712f504686ca543564
Adam Kliment <adam@netmilk.net>
2024-05-09 10:01:11 +0200

    actor.sh under .actor dir

---
+++ .actor/bin/actor.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#pwd
+#find ./storage
+apify actor:get-input > /dev/null
+INPUT=`apify actor:get-input | jq -r .urls[] | xargs echo`
+echo "INPUT: $INPUT"
+
+for url in $INPUT; do
+  # support for local usage
+  # sanitize url to a safe *nix filename - replace nonalfanumerical characters
+  # https://stackoverflow.com/questions/9847288/is-it-possible-to-use-in-a-filename
+  # https://serverfault.com/questions/348482/how-to-remove-invalid-characters-from-filenames
+  safe_filename=`echo $url | sed -e 's/[^A-Za-z0-9._-]/_/g'`
+  echo "Monolith-ing $url to key $safe_filename"
+  monolith $url | apify actor:set-value "$safe_filename" --contentType=text/html 
+done
+
+exit 0
