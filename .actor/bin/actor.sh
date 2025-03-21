d5860968b3fcda8a5a8b81034c1655442317711a
Adam Kliment <adam@netmilk.net>
2025-03-21 15:50:08 +0100

    Fixed actor.json, added dataset schema

--- .actor/bin/actor.sh
+++ .actor/bin/actor.sh
@@ -12,7 +12,11 @@ for url in $INPUT; do
   # https://serverfault.com/questions/348482/how-to-remove-invalid-characters-from-filenames
   safe_filename=`echo $url | sed -e 's/[^A-Za-z0-9._-]/_/g'`
   echo "Monolith-ing $url to key $safe_filename"
-  monolith $url | apify actor:set-value "$safe_filename" --contentType=text/html 
+  monolith $url | apify actor:set-value "$safe_filename" --contentType=text/html
+  kvs_url="https://api.apify.com/v2/key-value-stores/${APIFY_DEFAULT_KEY_VALUE_STORE_ID}/records/${safe_filename}"
+  result=$?
+  echo "Pushing result item to the datastore"
+  echo "{\"url\":\"${url}\",\"status\":\"${result}\", \"kvsUrl\":\"${kvs_url}\"}" | apify actor:push-data
 done
 
 exit 0
